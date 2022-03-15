6012a97d582614c4cd44e19aaa0e95affaa35f66
aishahsofea <aishah.s.hanifa@gmail.com>
2022-03-16 00:01:18 +0800

    first commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+    mapping(address => uint256) public wavesPerAddress;
+
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        wavesPerAddress[msg.sender] += 1;
+        console.log("%s has waved!", msg.sender);
+        console.log(
+            "%s waved %d times",
+            msg.sender,
+            wavesPerAddress[msg.sender]
+        );
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
